391a92f5b070e6574ec4f96e3a8d805f6b81d413
Nicolas <Nicolas@10.40.2.62>
2019-05-28 08:08:33 +0200

    Changed rotation method.

--- src/tetris/blocks/Block.java
+++ src/tetris/blocks/Block.java
@@ -5,11 +5,27 @@ import engine.objects.GameObject;
 public abstract class Block extends GameObject {
 	
 	public void rotateLeft() {
-		this.changeRotation(-90);
+		if(this.rotation.z == 0) {
+			rotation.z = 90;
+		}else if(rotation.z == 90) {
+			rotation.z = 180;
+		}else if(rotation.z == 180) {
+			rotation.z = 270;
+		}else if(rotation.z == 270) {
+			rotation.z = 0;
+		}
 	}
 	
 	public void rotateRight() {
-		this.changeRotation(90);
+		if(this.rotation.z == 0) {
+			rotation.z = -90;
+		}else if(rotation.z == -90) {
+			rotation.z = -180;
+		}else if(rotation.z == -180) {
+			rotation.z = -270;
+		}else if(rotation.z == -270) {
+			rotation.z = 0;
+		}
 	}
 	
 }
