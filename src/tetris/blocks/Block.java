c091e8165b23d9786bdd1d2eb4582da2b8e3cda3
Nicolas <Nicolas@10.40.2.62>
2019-05-28 10:11:27 +0200

    Changed rotation method.

--- src/tetris/blocks/Block.java
+++ src/tetris/blocks/Block.java
@@ -1,30 +1,31 @@
 package tetris.blocks;
 
+import engine.math.Vector3f;
 import engine.objects.GameObject;
 
 public abstract class Block extends GameObject {
 	
 	public void rotateLeft() {
-		if(this.rotation.z == 0) {
-			rotation.z = 90;
-		}else if(rotation.z == 90) {
-			rotation.z = 180;
-		}else if(rotation.z == 180) {
-			rotation.z = 270;
-		}else if(rotation.z == 270) {
-			rotation.z = 0;
+		if((int)this.rotation.z == 0) {
+			this.setRotation(new Vector3f(0,0,-90));
+		}else if((int)rotation.z == -90) {
+			this.setRotation(new Vector3f(0,0,-180));
+		}else if((int)rotation.z == -180) {
+			this.setRotation(new Vector3f(0,0,-270));
+		}else if((int)rotation.z == -270) {
+			this.setRotation(new Vector3f(0,0,0));
 		}
 	}
 	
 	public void rotateRight() {
-		if(this.rotation.z == 0) {
-			rotation.z = -90;
-		}else if(rotation.z == -90) {
-			rotation.z = -180;
-		}else if(rotation.z == -180) {
-			rotation.z = -270;
-		}else if(rotation.z == -270) {
-			rotation.z = 0;
+		if((int)this.rotation.z == 0) {
+			this.setRotation(new Vector3f(0,0,90));
+		}else if((int)rotation.z == 90) {
+			this.setRotation(new Vector3f(0,0,180));
+		}else if((int)rotation.z == 180) {
+			this.setRotation(new Vector3f(0,0,270));
+		}else if((int)rotation.z == 270) {
+			this.setRotation(new Vector3f(0,0,0));
 		}
 	}
 	
