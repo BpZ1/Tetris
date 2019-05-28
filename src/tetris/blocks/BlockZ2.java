23666220f2dfbc41cbce82e2f47588d733bd8b36
Nicolas <Nicolas@10.40.2.62>
2019-05-28 08:08:48 +0200

    Added Z Blocks.

--- src/tetris/blocks/BlockZ2.java
+++ src/tetris/blocks/BlockZ2.java
@@ -4,12 +4,12 @@ import engine.objects.GameObject;
 import engine.textures.Sprite;
 import engine.textures.manager.TextureManager;
 
-public class BlockZ1 extends Block {
-
-	private static final String TEXTURE_PATH = "res/blockRed.png";
+public class BlockZ2 extends Block {
+	
+	private static final String TEXTURE_PATH = "res/blockViolett.png";
 	private Sprite sprite;
 	
-	public BlockZ1() {
+	public BlockZ2() {
 		sprite = TextureManager.getSprite(TEXTURE_PATH, 0, 0);
 		
 		SubBlock block1 = new SubBlock(sprite);
@@ -17,7 +17,7 @@ public class BlockZ1 extends Block {
 		this.addChild(block1);
 		
 		SubBlock block2 = new SubBlock(sprite);
-		block2.setPosition(-SubBlock.getWidth(), 0);
+		block2.setPosition(SubBlock.getWidth(), 0);
 		this.addChild(block2);
 		
 		SubBlock block3 = new SubBlock(sprite);
@@ -25,7 +25,7 @@ public class BlockZ1 extends Block {
 		this.addChild(block3);
 		
 		SubBlock block4 = new SubBlock(sprite);
-		block4.setPosition(SubBlock.getWidth(), -SubBlock.getWidth());
+		block4.setPosition(-SubBlock.getWidth(), -SubBlock.getWidth());
 		this.addChild(block4);
 		
 	}
